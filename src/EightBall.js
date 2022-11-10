cc654452254588f71d409b9aa6c06822ea4d1b68
ollie <olivinanzali07@gmail.com>
2022-11-09 21:08:12 -0600

    adding beginning of EightBall

---
+++ src/EightBall.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const EightBall = (props) => {
+    const [msg, setMsg] = useState("Think of a Question.");
+    const [color, setColor] = useState("black");
+
+const EightBallAnswers = () => {
+   let answer = [
+        { msg: "It is certain.", color: "green" },
+        { msg: "Without a doubt.", color: "green" },
+        { msg: "It is decidedly so.", color: "green" },
+        { msg: "Yes - definitely.", color: "green" },
+        { msg: "You may rely on it.", color: "green" },
+        { msg: "As I see it, yes.", color: "green" },
+        { msg: "Most likely.", color: "green" },
+        { msg: "Outlook good.", color: "green" },
+        { msg: "Yes.", color: "green" },
+        { msg: "Signs point to yes.", color: "goldenrod" },
+        { msg: "Reply hazy, try again.", color: "goldenrod" },
+        { msg: "Ask again later.", color: "goldenrod" },
+        { msg: "Better not tell you now.", color: "goldenrod" },
+        { msg: "Cannot predict now.", color: "goldenrod" },
+        { msg: "Concentrate and ask again.", color: "goldenrod" },
+        { msg: "Don't count on it.", color: "red" },
+        { msg: "My reply is no.", color: "red" },
+        { msg: "My sources say no.", color: "red" },
+        { msg: "Outlook not so good.", color: "red" },
+        { msg: "Very doubtful.", color: "red" },
+    ]
+
+    const rando = Math.floor(Math.random() * answer.length);
+    return rando;
+    }
+}
+
+export default EightBall;
